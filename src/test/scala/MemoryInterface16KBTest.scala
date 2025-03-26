11e5143bb282bf4822be8f5ddff1cb8ea037ce5a
AsgerWenneb <asger@wenneberg.net>
2025-03-26 11:27:59 +0100

    pipecone2memory interface

---
+++ src/test/scala/MemoryInterface16KBTest.scala
@@ -0,0 +1,67 @@
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class MemoryInterface extends AnyFlatSpec with ChiselScalatestTester {
+    behavior of "Memory Interface"
+
+    it should "Write; acknowledge and set cs" in {
+        test(new MemoryInterface16KB(32)) {
+          (dut => {
+                // Write enable
+                dut.io.pipe.wr.poke(true.B)
+
+                // Don't acknowledge the same clock cycle
+                dut.io.pipe.ack.expect(false.B)
+                dut.io.memcs.expect(true.B)
+
+                // Acknowledge the following clock cycle
+                dut.clock.step(1)
+                dut.io.pipe.wr.poke(false.B)
+                dut.io.pipe.ack.expect(true.B)
+                dut.io.memcs.expect(false.B)
+
+                // Set acknowledge low after no transaction
+                dut.clock.step(1)
+                dut.io.pipe.ack.expect(false.B)
+            })
+        }
+    }
+
+    it should "Read; acknowledge and set cs" in {
+        test(new MemoryInterface16KB(32)) {
+            dut => {
+                // Write enable
+                dut.io.pipe.rd.poke(true.B)
+
+                // Don't acknowledge the same clock cycle
+                dut.io.pipe.ack.expect(false.B)
+
+                // Set cs high
+                dut.io.memcs.expect(true.B)
+
+                // Acknowledge the following clock cycle
+                // and set memcs low
+                dut.clock.step(1)
+                dut.io.pipe.rd.poke(false.B)
+                dut.io.pipe.ack.expect(true.B)
+                dut.io.memcs.expect(false.B)
+
+                // Set acknowledge low after no transaction
+                dut.clock.step(1)
+                dut.io.pipe.ack.expect(false.B)
+            }
+        }
+    }
+
+    it should "Slice the first 12 bits of the address" in {
+        test(new MemoryInterface16KB(32)) {
+            dut => {
+                // Input 32bit
+                dut.io.pipe.address.poke("hDEADBEEF".U(32.W))
+
+                dut.io.memaddress.expect("hEEF".U(12.W))
+            }
+        }
+    }
+}
