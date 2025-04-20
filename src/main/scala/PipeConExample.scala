22491688b3ea7494bff6b8acbbe1e6e30f6c3032
Rasmus <rasmuslindholdt2406@gmail.com>
2025-04-20 17:32:08 +0200

    connection between sink error... flipped pipecon is not correct

--- src/main/scala/PipeConExample.scala
+++ src/main/scala/PipeConExample.scala
@@ -3,68 +3,63 @@ import chisel3._
 
 class PipeConExample(addrWidth: Int) extends Module {
   val io = IO(new Bundle {
-    val UART   = Flipped(new PipeCon(addrWidth))
-//    val SPI    = new PipeCon(addrWidth)
-//    val SPImem = new PipeCon(addrWidth)
-//    val VGA    = new PipeCon(addrWidth)
-//    val KBD    = new PipeCon(addrWidth)
-//    val Mem    = new PipeCon(addrWidth)
-//    val IOcell = new PipeCon(addrWidth)
+    val UART   = (new PipeCon(addrWidth))
+    //val SPI    = (new PipeCon(addrWidth))
+    //val SPImem = (new PipeCon(addrWidth))
+    //val VGA    = (new PipeCon(addrWidth))
+    //val KBD    = (new PipeCon(addrWidth))
+    //val Mem    = (new PipeCon(addrWidth))
+    //val IOcell = (new PipeCon(addrWidth))
   })
+
+  // Default assignments for all PipeCon interfaces
+for (iface <- Seq(io.UART)) {//, io.SPI, io.SPImem, io.VGA, io.KBD, io.Mem, io.IOcell)) {
+  //iface.ack    := false.B
+  //iface.rdData := 0.U
+  iface.wrData := 0.U
+  iface.wrMask := VecInit(Seq(false.B, false.B, false.B, false.B))
+  iface.address := 0.U
+  iface.rd      := false.B
+  iface.wr      := false.B
+}
 
   val cpu = Module(new ThreeCats())
   
-
   cpu.io.imem.stall  := false.B  
   cpu.io.dmem.stall  := false.B  
   cpu.io.imem.data   := "h00000000".U(32.W)  
   cpu.io.dmem.rdData := "h00000000".U(32.W)
 
-  
-  val wrDataCPU   = Wire(UInt(32.W))
-  val wrMaskCPU   = Wire(Vec (4, Bool()))
-  val wrAddrCPU   = Wire(UInt(32.W))  
-  val rdAddrCPU   = Wire(UInt(32.W))  
+  val wrDataCPU   = IO(UInt(32.W))
+  val wrMaskCPU   = IO(Vec(4, Bool()))
+  val wrAddrCPU   = IO(UInt(32.W))  
+  val rdAddrCPU   = IO(UInt(32.W))  
+
   val rdEnableCPU = WireDefault(false.B)  
 
-  
-  
-  
   wrDataCPU   := cpu.io.dmem.wrData
   wrMaskCPU   := cpu.io.dmem.wrEnable
   wrAddrCPU   := cpu.io.dmem.wrAddress  
   rdAddrCPU   := cpu.io.dmem.rdAddress  
   rdEnableCPU := cpu.io.dmem.rdEnable  
-  
 
   // Simple memory to simulate a read/write behavior
   val mem = Mem(256, UInt(32.W))  // 256 locations, 32 bits wide
 
-  //io.UART.rdData := 0.U  // Default read data = 0
-  //io.UART.ack := false.B // Default acknowledge = false
-
-
-  
-
+  // Write to UART if the mask is not zero
   when(wrMaskCPU.asUInt =/= "b0000".U) {
-    io.UART.wr := true.B
-    io.UART.wrMask := wrMaskCPU
-    for(i <- 0 until addrWidth) {
-      if (wrAddrCPU(31-i) == "b1") {
-        io.UART.wrData := wrDataCPU
-        io.UART.address := wrAddrCPU
-        
-      }
+    when(wrAddrCPU === 0x00000001.U) {  // Write to UART address
+      io.UART.wrData := wrDataCPU
+      //io.UART.address := wrAddrCPU  
     }
   }
 
-
-  //io.UART.rdData := wrDataCPU  // Example read operation
-  //io.UART.ack := true.B  // Example write acknowledgment
-
+  // Read from UART
+  //when(rdEnableCPU) {
+  //  io.UART.rdData := mem(rdAddrCPU)  // Simulate a memory read to UART
+  //}
 }
 
 object PipeConExample extends App {
-  // Use ChiselStage instead of Driver
-  (new chisel3.stage.ChiselStage).emitVerilog(new PipeConExample(8))  // For example, 8-bit address width
+  (new chisel3.stage.ChiselStage).emitVerilog(new PipeConExample(8))
 }
--- src/test/scala/PipeConTest.scala
+++ src/test/scala/PipeConTest.scala
@@ -4,15 +4,15 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class PipeConTest extends AnyFlatSpec with ChiselScalatestTester {
   "PipeConExample" should "perform read and write operations correctly" in {
-    test(new PipeConExample(8)) { c =>
-      c.wrDataCPU.poke(0x00001111.U)
-      c.wrMaskCPU.poke(VecInit(Seq(true.B, true.B, false.B, false.B)))
+    test(new PipeConExample(8)) { c => 
+      // Step the clock
+      def step() = c.clock.step()
 
+      // Write some data to the UART address
+      //c.io.UART.wrData.poke(0x12345678.U)   // Write data to wrData
+      //c.io.UART.wrMask(0).poke(true.B)      // Set write mask to write the first byte
+      //c.io.UART.wr.poke(true.B)             // Assert write signal
 
-
-      c.io.UART.rdData.expect("h00001111".U(32.W))
-
-      
     }
   }
 }
