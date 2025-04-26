640219b5031e0d88094137dde21eb35e2df48e7d
jjdehl <jacobjdehlsen@gmail.com>
2025-04-26 11:16:35 +0200

    vh header file gen

---
+++ src/main/scala/pyaHardwareGen/vh_port_config.py
@@ -0,0 +1,44 @@
+import csv
+import sys
+
+modulename = "t"
+
+power_def_string = """`ifdef USE_POWER_PINS
+    inout VPWR,
+    inout VGND,
+`endif
+"""
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Wrong number of args")
+        return 1
+
+    port_config = sys.argv[1]
+
+    output = []
+    output.append("module " + modulename + " (\n")
+    output.append(power_def_string)
+    output.append("\n")
+
+    with open(port_config, "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+
+        for row in reader:
+            if row[0] in ["VPWR", "VGND"]:
+                continue
+            else:
+                output.append("   " + row[1].lower() + " logic " + row[0] + ",\n")
+    
+    output.append(");\n")
+    output.append("\n")
+    output.append("endmodule\n")
+
+    vh_out = modulename + ".vh"
+    with open(vh_out, "w") as lef:
+        lef.writelines(output)
+
+
+if __name__ == "__main__":
+    main()
