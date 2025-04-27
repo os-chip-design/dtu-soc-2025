03b981167ff3b0086633e3981b39836d7fd4d73f
Stephan <s233451@student.dtu.dk>
2025-04-27 11:48:14 +0200

    Group 1: Start of register organisation

--- src/main/scala/GPIO/GPIOPeripheral.scala
+++ src/main/scala/GPIO/GPIOPeripheral.scala
@@ -6,71 +6,103 @@ import chisel3.util._
 // the PipeCon interface is used to allow the CPU to
 // configure and write/read from the GPIO and PWM peripherals
 
-class GPIOPeripheral(addrWidth: Int) extends Module {
+class GPIOPeripheral(addrWidth: Int, nofGPIO: Int) extends Module {
 
     // Instantiate the Pipecon interface
     val io = IO(new Bundle {
         val mem_ifc = (new PipeCon(addrWidth)) // From CPU
     })
     // Register address offsets
-    val GPIO_REG_OFFSET         = 0x0000.U(addrWidth.W) // Start addr of GPIO registers
-    val PWM_REG_OFFSET          = 0x0400.U(addrWidth.W) // Start addr of PWM registers
+    val GPIO_REG_OFFSET         = 0x0000 // Start addr of GPIO registers
+    val PWM_REG_OFFSET          = 0x0400 // Start addr of PWM registers
 
     // GPIO registers offsets within GPIO address space
-    val GPIO_DIRECTION_OFFSET       = 0x0000.U(addrWidth.W) // GPIO direction register
-    val GPIO_OUTPUT_OFFSET          = 0x0008.U(addrWidth.W) // GPIO output register
-    val GPIO_INPUT_OFFSET           = 0x0010.U(addrWidth.W) // GPIO input register
-    val GPIO_PULLUP_OFFSET          = 0x0018.U(addrWidth.W) // GPIO pullup register
-    val GPIO_PULLDOWN_OFFSET        = 0x0020.U(addrWidth.W) // GPIO pulldown register
-    val GPIO_OPENDRAIN_OFFSET       = 0x0028.U(addrWidth.W) // GPIO open drain register
-    val GPIO_DRIVESTRENGTH_OFFSET   = 0x0030.U(addrWidth.W) // GPIO drive strength register
+    val GPIO_DIRECTION_OFFSET       = (GPIO_REG_OFFSET | 0x0000) // GPIO direction register
+    val GPIO_OUTPUT_OFFSET          = (GPIO_REG_OFFSET | 0x0008) // GPIO output register
+    val GPIO_INPUT_OFFSET           = (GPIO_REG_OFFSET | 0x0010) // GPIO input register
+    val GPIO_PULLUP_OFFSET          = (GPIO_REG_OFFSET | 0x0018) // GPIO pullup register
+    val GPIO_PULLDOWN_OFFSET        = (GPIO_REG_OFFSET | 0x0020) // GPIO pulldown register
+    val GPIO_OPENDRAIN_OFFSET       = (GPIO_REG_OFFSET | 0x0028) // GPIO open drain register
+    val GPIO_DRIVESTRENGTH_OFFSET   = (GPIO_REG_OFFSET | 0x0030) // GPIO drive strength register
 
     // PWM registers offsets within PWM address space
-    val PWM_ENABLE_OFFSET          = 0x0000.U(addrWidth.W) // PWM enable register
-    val PWM_PERIOD_OFFSET          = 0x0008.U(addrWidth.W) // PWM period register
-    val PWM_DUTY_CYCLE_OFFSET      = 0x0010.U(addrWidth.W) // PWM duty cycle register    
-    val PWM_PRESCALER_OFFSET       = 0x0018.U(addrWidth.W) // PWM prescaler register
-    val PWM_POLARITY_OFFSET        = 0x0020.U(addrWidth.W) // PWM polarity register
-
-    // Default values, inputs 
-    //io.mem_ifc.address      := 0.U
-    //io.mem_ifc.rd           := false.B
-    //io.mem_ifc.wr           := false.B
-    //io.mem_ifc.wrData       := 0.U
-    //io.mem_ifc.wrMask       := "b0000".U
+    val PWM_ENABLE_OFFSET          = (PWM_REG_OFFSET | 0x0000) // PWM enable register
+    val PWM_PERIOD_OFFSET          = (PWM_REG_OFFSET | 0x0008) // PWM period register
+    val PWM_DUTY_CYCLE_OFFSET      = (PWM_REG_OFFSET | 0x0010) // PWM duty cycle register    
+    val PWM_PRESCALER_OFFSET       = (PWM_REG_OFFSET | 0x0018) // PWM prescaler register
+    //val PWM_POLARITY_OFFSET        = 0x0020.U(addrWidth.W) // PWM polarity register
     
     // Default values, outputs
     io.mem_ifc.ack          := false.B
     io.mem_ifc.rdData       := 0.U
 
-    // Mapping read address to registers
-    val readRegAddr = io.mem_ifc.address - GPIO_REG_OFFSET
+    // Instantiation of the GPIO registers
+    val gpio_direction     = RegInit(0x0.U(nofGPIO.W))
+    val gpio_output        = RegInit(0xF.U(nofGPIO.W))
+    val gpio_input         = RegInit(0.U(nofGPIO.W))
+    val gpio_pullup        = RegInit(0.U(nofGPIO.W))
+    val gpio_pulldown      = RegInit(0.U(nofGPIO.W))
+    val gpio_opendrain     = RegInit(0.U(nofGPIO.W))
+    val gpio_drivestrength = RegInit(0.U(nofGPIO.W))
 
-    switch (readRegAddr) {
-        is(GPIO_DIRECTION_OFFSET) {
-            io.mem_ifc.rdData := 1.U // Read GPIO direction register
-        }
-        is(GPIO_OUTPUT_OFFSET) {
-            io.mem_ifc.rdData := 2.U // Read GPIO output register
-        }
-        is(GPIO_INPUT_OFFSET) {
-            io.mem_ifc.rdData := 3.U // Read GPIO input register
-        }
-        is(GPIO_PULLUP_OFFSET) {
-            io.mem_ifc.rdData := 4.U // Read GPIO pullup register
-        }
-        is(GPIO_PULLDOWN_OFFSET) {
-            io.mem_ifc.rdData := 5.U // Read GPIO pulldown register
-        }
-        is(GPIO_OPENDRAIN_OFFSET) {
-            io.mem_ifc.rdData := 6.U // Read GPIO open drain register
-        }
-        is(GPIO_DRIVESTRENGTH_OFFSET) {
-            io.mem_ifc.rdData := 7.U // Read GPIO drive strength register
-        }
+    val pwm_enable         = RegInit(0.U(nofGPIO.W))
+    val pwm_duty_cycle     = RegInit(0.U((8*nofGPIO).W))
+    val pwm_prescaler      = RegInit(0.U((8*nofGPIO).W))
+    val pwm_div            = RegInit(0.U((8*nofGPIO).W))
+
+    // Instantiation of GPIO modules
+    val gpio_module = Seq.fill(nofGPIO)(Module(new GPIOModule))
+
+    // Mapping the GPIO modules to the corresponding registers
+    for (i <- 0 until nofGPIO) {
+        gpio_module(i).io.gpio_direction     := gpio_direction(i)
+        gpio_module(i).io.gpio_output        := gpio_output(i)
+        gpio_module(i).io.drivestrength      := gpio_drivestrength(i)
+        gpio_module(i).io.pullup_en          := gpio_pullup(i)
+        gpio_module(i).io.pulldown_en        := gpio_pulldown(i)
+        gpio_module(i).io.opendrain_en       := gpio_opendrain(i)
+        gpio_module(i).io.pwm_en             := pwm_enable(i)
+        gpio_module(i).io.pwm_div            := pwm_div(i)
+        gpio_module(i).io.duty_cycle         := pwm_duty_cycle(i)
     }
 
+    // Piping read address to statemachine/registers
+    val readRegAddr = io.mem_ifc.address
 
-
-
+    // WIP:
+    switch (readRegAddr) {
+        is(GPIO_DIRECTION_OFFSET.U(addrWidth.W)) {
+            io.mem_ifc.rdData := gpio_direction // Read GPIO direction register
+        }
+        is(GPIO_OUTPUT_OFFSET.U(addrWidth.W)) {
+            io.mem_ifc.rdData := gpio_output // Read GPIO output register
+        }
+        is(GPIO_INPUT_OFFSET.U(addrWidth.W)) {
+            io.mem_ifc.rdData := gpio_input // Read GPIO input register
+        }
+        is(GPIO_PULLUP_OFFSET.U(addrWidth.W)) {
+            io.mem_ifc.rdData := gpio_pullup // Read GPIO pullup register
+        }
+        is(GPIO_PULLDOWN_OFFSET.U(addrWidth.W)) {
+            io.mem_ifc.rdData := gpio_pulldown // Read GPIO pulldown register
+        }
+        is(GPIO_OPENDRAIN_OFFSET.U(addrWidth.W)) {
+            io.mem_ifc.rdData := gpio_opendrain // Read GPIO open drain register
+        }
+        is(GPIO_DRIVESTRENGTH_OFFSET.U(addrWidth.W)) {
+            io.mem_ifc.rdData := gpio_drivestrength // Read GPIO drive strength register
+        }
+        is(PWM_ENABLE_OFFSET.U(addrWidth.W)) {
+            io.mem_ifc.rdData := pwm_enable // Read PWM enable register
+        }
+        // is(PWM_PERIOD_OFFSET.U(addrWidth.W)) {
+        //     io.mem_ifc.rdData := pwm_period // Read PWM period register
+        // }
+        is(PWM_DUTY_CYCLE_OFFSET.U(addrWidth.W)) {
+            io.mem_ifc.rdData := pwm_duty_cycle // Read PWM duty cycle register
+        }
+        is(PWM_PRESCALER_OFFSET.U(addrWidth.W)) {
+            io.mem_ifc.rdData := pwm_prescaler // Read PWM prescaler register
+        }
+    }
 }
--- src/test/scala/GPIO/GPIOPeripheralTest.scala
+++ src/test/scala/GPIO/GPIOPeripheralTest.scala
@@ -8,19 +8,31 @@ class GPIOPeripheralTest extends AnyFlatSpec with ChiselScalatestTester {
     it should "pass" in {
         test(
             new GPIOPeripheral(
-                    addrWidth = 32
+                    addrWidth = 32,
+                    nofGPIO = 8,
                 )
             ) {
             dut => {
                 
                 // Read register address space and check the rd values
-                for (i <- 0 until 7) {
-                    dut.io.mem_ifc.address.poke((i*8).U)
+                // for (i <- 0 until 7) {
+                //     dut.io.mem_ifc.address.poke((i*8).U)
 
-                    dut.io.mem_ifc.rdData.expect((i+1).U)
+                //     dut.io.mem_ifc.rdData.expect((i+1).U)
 
-                    dut.clock.step()
-                }
+                //     dut.clock.step()
+                // }
+
+                // Read some registers
+                dut.io.mem_ifc.address.poke(0x0000.U(32.W))
+                dut.io.mem_ifc.rdData.expect(0.U) // Check GPIO direction register
+                dut.clock.step()
+                dut.io.mem_ifc.address.poke(0x0008.U(32.W))
+                dut.io.mem_ifc.rdData.expect(0xF.U) // Check GPIO output register
+
+                //dut.gpio_module(0).io.gpio_output.expect(0.U) // Check GPIO output
+
+                dut.clock.step()
 
             }
         }
