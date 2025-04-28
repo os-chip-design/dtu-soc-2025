4c9914121209869ee28755829ae865ee29b9024b
Collinnn <tobias3060@gmail.com>
2025-04-28 23:48:17 +0200

    Added default values for certain stuff

--- src/main/scala/SpiOffChipMemoryG2/OffChipMemoryController.scala
+++ src/main/scala/SpiOffChipMemoryG2/OffChipMemoryController.scala
@@ -3,16 +3,33 @@ import chisel3.util._
 
 // Main module for the off-chip memory controller
 class OffChipMemoryController() extends Module {
+  
   val spiPort = IO(new spiIO)
   val pipeCon = IO(new PipeCon(24))
 
   val bridge = Module(new Bridge())
-  spiPort <> bridge.spiPort
+  
 
   val config = Module(new OffChipMemoryConfig())
+  
+  // Default values for the configuration
+  config.pipeCon.address := 0.U
+  config.pipeCon.rd      := 0.U
+  config.pipeCon.wr      := 0.U
+  config.pipeCon.wrData  := 0.U
+  config.pipeCon.wrMask  := 0.U
 
+  // Default values for the bridge
+  bridge.pipeCon.address := 0.U
+  bridge.pipeCon.rd      := 0.U
+  bridge.pipeCon.wr      := 0.U
+  bridge.pipeCon.wrData  := 0.U
+  bridge.pipeCon.wrMask  := 0.U
+  
   val configAddr = "h00000000".U(24.W)
+  spiPort <> bridge.spiPort
   bridge.config <> config.config
+
 
   when (pipeCon.address === configAddr){
     pipeCon <> config.pipeCon
