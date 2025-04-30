d0fe755bbe3d9fe6ba49ae210430ecbebf3060fb
Collinnn <tobias3060@gmail.com>
2025-04-30 15:16:18 +0200

    G2 extensive config testing

--- src/test/scala/SPIOffChipMemoryG2/configSpec.scala
+++ src/test/scala/SPIOffChipMemoryG2/configSpec.scala
@@ -59,19 +59,47 @@ class configSpec
       pipeCon.rd.poke(true.B)
       pipeCon.wr.poke(false.B)
       pipeCon.wrData.poke(0.U)
-      
       clock.step(1)
       assert(pipeCon.ack.peek().litToBoolean == true)
       assert(pipeCon.rdData.peek().litValue == 36)
       assert(config.jedec.peek().litToBoolean == false)
       assert(config.clear.peek().litToBoolean == false)
       assert(config.targetFlash.peek().litToBoolean == true)
-
-
-
+      clock.step(1)
+      pipeCon.rd.poke(false.B)
+      pipeCon.wr.poke(true.B)
+      pipeCon.wrData.poke("b00000000000000000000000000000001".U(32.W))
+      clock.step(1)
+      assert(pipeCon.ack.peek().litToBoolean == true)
+      assert(pipeCon.rdData.peek().litValue == 1)
+      assert(config.jedec.peek().litToBoolean == true)
+      assert(config.clear.peek().litToBoolean == false)
+      assert(config.targetFlash.peek().litToBoolean == false)
+      assert(config.clockDivision.peek().litValue == 0)
+      assert(config.mode.peek().litToBoolean == false)
+      clock.step(1)
+      pipeCon.wrData.poke("b10000000000000000000010100000110".U(32.W))
+      clock.step(1)
+      assert(pipeCon.ack.peek().litToBoolean == true)
+      assert(pipeCon.rdData.peek().litValue == "b10000000000000000000010100000110".U(32.W).litValue)
+      assert(config.jedec.peek().litToBoolean == false)
+      assert(config.clear.peek().litToBoolean == true)
+      assert(config.targetFlash.peek().litToBoolean == true)
+      assert(config.clockDivision.peek().litValue == 160)
+      assert(config.mode.peek().litToBoolean == true)
+      clock.step(1)
+      pipeCon.rd.poke(true.B)
+      pipeCon.wr.poke(false.B)
+      pipeCon.wrData.poke(0.U)
+      clock.step(1)
+      assert(pipeCon.ack.peek().litToBoolean == true)
+      assert(config.jedec.peek().litToBoolean == false)
+      assert(config.clear.peek().litToBoolean == true)
+      assert(pipeCon.rdData.peek().litValue == "b10000000000000000000010100000110".U(32.W).litValue)
+      assert(config.targetFlash.peek().litToBoolean == true)
+      assert(config.clockDivision.peek().litValue == 160)
+      assert(config.mode.peek().litToBoolean == true)
       
-
-
     }
   }
 }
