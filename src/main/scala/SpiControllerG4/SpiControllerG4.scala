8385ac1468eb9638fa128c755d61b7a1336d8545
Javad Asgari Shafique <me@javad.sh>
2025-04-30 14:05:18 +0200

    Move .v output to `generated`

--- src/main/scala/SpiControllerG4/SpiControllerG4.scala
+++ src/main/scala/SpiControllerG4/SpiControllerG4.scala
@@ -13,125 +13,127 @@ import chisel3.util._
 
 //Just a comment for Git test 
 class SpiControllerG4 extends Module {
-    val io = IO(new Bundle {
-        val cpuCommand = Input(UInt(8.W))
-        val cpuWriteData = Input(UInt(32.W))
-        val cpuReadData = Output(UInt(32.W))
-        val enable = Input(Bool())
-        val sendLength = Input(UInt(6.W))
-        val rw = Input(Bool()) // 0 when also reciving data, 1 for sending only
+  val io = IO(new Bundle {
+    val cpuCommand = Input(UInt(8.W))
+    val cpuWriteData = Input(UInt(32.W))
+    val cpuReadData = Output(UInt(32.W))
+    val enable = Input(Bool())
+    val sendLength = Input(UInt(6.W))
+    val rw = Input(Bool()) // 0 when also reciving data, 1 for sending only
 
-        val spiMiso = Input(Bool())
-        val spiMosi = Output(Bool())
-        val spiCs = Output(Bool())
-        val spiClk = Output(Bool())
+    val spiMiso = Input(Bool())
+    val spiMosi = Output(Bool())
+    val spiCs = Output(Bool())
+    val spiClk = Output(Bool())
 
-        val ready = Output(Bool())
-        
-        val prescale = Input(UInt(4.W))
-    })
+    val ready = Output(Bool())
 
-    object State extends ChiselEnum {
-        val idle, loadData, sendData, receiveData, deassertCS = Value
+    val prescale = Input(UInt(4.W))
+  })
+
+  object State extends ChiselEnum {
+    val idle, loadData, sendData, receiveData, deassertCS = Value
+  }
+
+  import State._
+
+  val stateReg = RegInit(idle)
+
+  val CNT_MAX = io.prescale - 1.U
+  val cntClk = RegInit(0.U(3.W))
+  val spiClkReg = RegInit(false.B)
+
+  val spiData = RegInit(0.U(32.W))
+  //val txBuffer = RegInit(0.U(32.W))
+  //val txBufferEmpty = RegInit(true.B)
+  val txShiftReg = RegInit(0.U(40.W))
+  //val txShiftRegEmpty = RegInit(true.B)
+
+  //val spiBuffer = RegInit(0.U(32.W))
+  //val rxBuffer = RegInit(0.U(32.W))
+  //val rxBufferEmpty = RegInit(true.B)
+  val rxShiftReg = RegInit(0.U(32.W))
+
+  val bitCounter = RegInit(0.U(8.W))
+
+  val csReg = RegInit(true.B)
+  io.spiCs := csReg
+
+  io.cpuReadData := 0.U
+  io.spiMosi := txShiftReg(39)
+  rxShiftReg := rxShiftReg | Cat("b0000000000000000000000000000000".U, io.spiMiso)
+
+  /*when (txBufferEmpty) {
+      txBuffer := spiData
+      txBufferEmpty := false.B
+  }
+
+  when (!rxBufferEmpty) {
+      spiBuffer := rxBuffer
+      rxBufferEmpty := true.B
+  }*/
+
+  when(io.prescale =/= 1.U) {
+    cntClk := cntClk + 1.U
+    when(cntClk === CNT_MAX) {
+      cntClk := 0.U
+      spiClkReg := !spiClkReg
     }
-    import State._
-    val stateReg = RegInit(idle)
+  }.otherwise {
+    spiClkReg := !spiClkReg
+  }
+  io.spiClk := spiClkReg
 
-    val CNT_MAX = io.prescale - 1.U
-    val cntClk = RegInit(0.U(3.W))
-    val spiClkReg = RegInit(false.B)
+  // Edge detection registers
+  val spiClkRegPrev = RegNext(spiClkReg)
+  val fallingEdge = !spiClkReg && spiClkRegPrev
+  val risingEdge = spiClkReg && !spiClkRegPrev
 
-    val spiData = RegInit(0.U(32.W))
-    //val txBuffer = RegInit(0.U(32.W))
-    //val txBufferEmpty = RegInit(true.B)
-    val txShiftReg = RegInit(0.U(40.W))
-    //val txShiftRegEmpty = RegInit(true.B)
-
-    //val spiBuffer = RegInit(0.U(32.W))
-    //val rxBuffer = RegInit(0.U(32.W))
-    //val rxBufferEmpty = RegInit(true.B)
-    val rxShiftReg = RegInit(0.U(32.W))
-
-    val bitCounter = RegInit(0.U(8.W))
-
-    val csReg = RegInit(true.B)
-    io.spiCs := csReg
-
-    io.cpuReadData := 0.U
-    io.spiMosi := txShiftReg(39)
-    rxShiftReg := rxShiftReg | Cat("b0000000000000000000000000000000".U, io.spiMiso)
-
-    /*when (txBufferEmpty) {
-        txBuffer := spiData
-        txBufferEmpty := false.B
+  switch(stateReg) {
+    is(idle) {
+      when(io.enable) {
+        stateReg := loadData
+      }
     }
-
-    when (!rxBufferEmpty) {
-        spiBuffer := rxBuffer
-        rxBufferEmpty := true.B
-    }*/
-
-    when (io.prescale =/= 1.U) {
-        cntClk := cntClk + 1.U
-        when (cntClk === CNT_MAX) {
-            cntClk := 0.U
-            spiClkReg := !spiClkReg
-        }
-    } .otherwise {
-        spiClkReg := !spiClkReg
+    is(loadData) {
+      csReg := false.B
+      txShiftReg := Cat(io.cpuWriteData, 0.U(8.W))
+      bitCounter := io.sendLength
+      stateReg := sendData
     }
-    io.spiClk := spiClkReg
+    is(sendData) {
+      when(fallingEdge) {
+        txShiftReg := txShiftReg << 1
+        bitCounter := bitCounter - 1.U
 
-    // Edge detection registers
-    val spiClkRegPrev = RegNext(spiClkReg)
-    val fallingEdge = !spiClkReg && spiClkRegPrev
-    val risingEdge = spiClkReg && !spiClkRegPrev
-
-    switch (stateReg) {
-        is (idle) {
-            when (io.enable) {
-                stateReg := loadData
-            }
+        when(bitCounter === 0.U) {
+          stateReg := Mux(!io.rw, receiveData, deassertCS)
         }
-        is (loadData) {
-            csReg := false.B
-            txShiftReg := Cat(io.cpuWriteData, 0.U(8.W))
-            bitCounter := io.sendLength
-            stateReg := sendData
-        }
-        is (sendData) {
-            when (fallingEdge) {
-                txShiftReg := txShiftReg << 1
-                bitCounter := bitCounter - 1.U
-
-                when (bitCounter === 0.U) {
-                    stateReg := Mux(!io.rw, receiveData, deassertCS)
-                }
-            }
-        }
-        is (receiveData) {
-            when(risingEdge){
-                rxShiftReg := rxShiftReg << 1
-                bitCounter := bitCounter + 1.U
-                when(bitCounter === 32.U) {
-                    io.cpuReadData := rxShiftReg
-                    stateReg := deassertCS
-                }
-            }
-        }
-        is (deassertCS) {
-            csReg := true.B
-            stateReg := idle
-        }
+      }
     }
-
-    when (stateReg === idle) {
-        io.ready := true.B
-    } .otherwise {
-        io.ready := false.B
+    is(receiveData) {
+      when(risingEdge) {
+        rxShiftReg := rxShiftReg << 1
+        bitCounter := bitCounter + 1.U
+        when(bitCounter === 32.U) {
+          io.cpuReadData := rxShiftReg
+          stateReg := deassertCS
+        }
+      }
     }
+    is(deassertCS) {
+      csReg := true.B
+      stateReg := idle
+    }
+  }
+
+  when(stateReg === idle) {
+    io.ready := true.B
+  }.otherwise {
+    io.ready := false.B
+  }
 }
 
 object SpiController extends App {
-  (new chisel3.stage.ChiselStage).emitVerilog(new SpiControllerG4())
+  (new chisel3.stage.ChiselStage).emitVerilog(new SpiControllerG4(), Array("--target-dir", "generated"))
 }
