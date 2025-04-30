86fd0df5316b3b3f2d9789f411a486d65b13dcdc
AndreasSvarta <andreas_svarta@hotmail.com>
2025-04-30 18:01:39 +0200

    svart skal på ubuntu

--- src/main/scala/PipeConInterconnect.scala
+++ src/main/scala/PipeConInterconnect.scala
@@ -2,139 +2,62 @@ import chisel3._
 import chisel3.util._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
-import wildcat.pipeline.{InstructionROM, MemIO, ThreeCats}
+import wildcat.pipeline._
 import wildcat.Util
 
-class PipeConInterconnect(addrWidth: Int) extends Module {
+class PipeConInterconnect(file: String, addrWidth: Int) extends Module {
   val io = IO(new Bundle {
-    val uart = new PipeCon(addrWidth)
-    val SPI  = new PipeCon(addrWidth)
-    val rdEnableTest = Output(Bool())
+    val uart = Flipped(new PipeCon(addrWidth))
+    //val SPI  = new PipeCon(addrWidth)
   })
 
-  // === Instantiate CPU and ROM ===
+  val (memory, start) = Util.getCode(file)
+
+  // Here switch between different designs
   val cpu = Module(new ThreeCats())
-
-  val testProgram = Array(
-    0x00100093, // li x1, 1
-    0x00200113, // li x2, 2
-    0x0000A183, // lw x3, 0(x1)
-    0x00310023, // sw x3, 0(x1) (Store word in x3 to address 0x01, i.e., UART)
-    0xDEADBEEF,  // Write 0xDEADBEEF directly to UART address 0x01
-    0x0000006F   // Loop
-  )
-
-  val paddedProgram = testProgram ++ Array.fill(251)(0x00000013)
-  val imem = Module(new InstructionROM(paddedProgram))
-  cpu.io.imem <> imem.io
-
-  //cpu.io.imem.stall := false.B
+  // val cpu = Module(new WildFour())
+  // val cpu = Module(new StandardFive())
+  val dmem = Module(new PipeConMem(memory))
+  cpu.io.dmem <> dmem.io
+  val imem = Module(new PipeConMemory(memory))
+  imem.io.address := cpu.io.imem.address
+  cpu.io.imem.data := imem.io.data
+  cpu.io.imem.stall := imem.io.stall
   cpu.io.dmem.stall := false.B
-  //cpu.io.imem.data := 0.U
 
-  //printf("Instruction: 0x%x, Write Address: 0x%x, Write Data: 0x%x\n", 
-  //  testProgram(3).U, cpu.io.dmem.wrAddress, cpu.io.dmem.wrData)
-  printf("Instruction Fetch: 0x%x\n", cpu.io.imem.address)
+  io.uart.rd := false.B
+  io.uart.wr := false.B
+  io.uart.address := 0.U
+  io.uart.wrData := 0.U
+  io.uart.wrMask := 0.U
 
-
-  // === rdEnable output ===
-  io.rdEnableTest := cpu.io.dmem.rdEnable
-
-  // === Instantiate Peripherals ===
-  val uartPeripheral = Module(new UARTPeripheral(addrWidth))
-  val spiPeripheral  = Module(new SPIPeripheral(addrWidth))
-
-  // === Default peripheral I/Os ===
-  uartPeripheral.io.wr := io.uart.wr
-  uartPeripheral.io.rd := io.uart.rd
-  uartPeripheral.io.wrMask := 0.U
-  uartPeripheral.io.wrData := 0.U
-  uartPeripheral.io.address := 0.U
-  uartPeripheral.testIo.testWrData := io.uart.wrData
-
-  spiPeripheral.io.wr := false.B
-  spiPeripheral.io.rd := false.B
-  spiPeripheral.io.wrMask := 0.U
-  spiPeripheral.io.wrData := 0.U
-  spiPeripheral.io.address := 0.U
-  spiPeripheral.testIo.testRdData := 0.U
-
-  // === External IO defaults ===
-  io.uart.rdData := 0.U
-  io.uart.ack    := false.B
-  io.SPI.rdData  := 0.U
-  io.SPI.ack     := false.B
-
-  // === Address Map ===
-  val uartAddress = "h02".U(addrWidth.W)
-  val spiAddress  = "h03".U(addrWidth.W)
-
-  // === CPU signals ===
-  val isRead  = cpu.io.dmem.rdEnable
-  val isWrite = cpu.io.dmem.wrEnable.reduce(_ || _)
-  printf("CPU Write Enable: %b\n", isWrite)
-
-
-  // Register them
-  val rdAddrReg = RegNext(cpu.io.dmem.rdAddress)
-  val wrAddrReg = RegNext(cpu.io.dmem.wrAddress)
-  val wrDataReg = RegNext(cpu.io.dmem.wrData)
-  val wrMaskReg = RegNext(cpu.io.dmem.wrEnable.asUInt)
-
-  val doReadReg  = RegNext(isRead)
-  val doWriteReg = RegNext(isWrite)
-
-  // Read Data Register
   val rdDataReg = RegInit(0.U(32.W))
+  val stall = RegInit(false.B)
 
-  // === Write Logic ===
-  printf("Write Address: 0x%x, UART Address: 0x%x, SPI Address: 0x%x\n", 
-    cpu.io.dmem.wrAddress, uartAddress, spiAddress)
 
-  when (isWrite) {
-    printf("CPU Write Enable: %d, Write Mask: 0x%x\n", isWrite, cpu.io.dmem.wrEnable.asUInt)
-    printf("CPU Write Address: 0x%x\n", cpu.io.dmem.wrAddress)
-    when (cpu.io.dmem.wrAddress === uartAddress) {
-      printf("CPU Writing to UART: Addr = 0x%x, Data = 0x%x\n", 
-        cpu.io.dmem.wrAddress, cpu.io.dmem.wrData)
-      uartPeripheral.io.wr := true.B
-      uartPeripheral.io.address := cpu.io.dmem.wrAddress
-      uartPeripheral.io.wrData := cpu.io.dmem.wrData
-      uartPeripheral.io.wrMask := cpu.io.dmem.wrEnable.asUInt
-    } .elsewhen (cpu.io.dmem.wrAddress === spiAddress) {
-      printf("CPU Writing to SPI: Addr = 0x%x, Data = 0x%x\n", 
-        cpu.io.dmem.wrAddress, cpu.io.dmem.wrData)
-      spiPeripheral.io.wr := true.B
-      spiPeripheral.io.address := cpu.io.dmem.wrAddress
-      spiPeripheral.io.wrData := cpu.io.dmem.wrData
-      spiPeripheral.io.wrMask := cpu.io.dmem.wrEnable.asUInt
+  val uartAddress = "h00000004".U
+
+  when(cpu.io.dmem.wrEnable.reduce(_ || _)) {
+    when(cpu.io.dmem.wrAddress === uartAddress) {
+      io.uart.wr := true.B
+      io.uart.rd := false.B
+      io.uart.address := cpu.io.dmem.wrAddress
+      io.uart.wrData := cpu.io.dmem.wrData
+      io.uart.wrMask := cpu.io.dmem.wrEnable.asUInt
     }
-  }
-
-
-  // === Read Logic ===
-  when (doReadReg) {
-    when (rdAddrReg === uartAddress) {
-      uartPeripheral.io.rd := true.B
-      uartPeripheral.io.address := rdAddrReg
-      rdDataReg := uartPeripheral.io.rdData
-    } .elsewhen (rdAddrReg === spiAddress) {
-      spiPeripheral.io.rd := true.B
-      spiPeripheral.io.address := rdAddrReg
-      rdDataReg := spiPeripheral.io.rdData
+  } .elsewhen(cpu.io.dmem.rdEnable) {
+    when(cpu.io.dmem.rdAddress === uartAddress) {
+      io.uart.rd := true.B
+      io.uart.wr := false.B
+      io.uart.address := cpu.io.dmem.rdAddress
+      rdDataReg := io.uart.rdData
     } .otherwise {
-      rdDataReg := 0.U
+      io.uart.address := cpu.io.dmem.wrAddress
     }
-  } .otherwise {
-    rdDataReg := 0.U
   }
-
-  // === Connect to CPU ===
+  
+  cpu.io.dmem.stall := stall
   cpu.io.dmem.rdData := rdDataReg
 
-  // === Connect external IOs ===
-  io.uart.rdData := uartPeripheral.io.rdData
-  io.uart.ack    := uartPeripheral.io.ack
-  io.SPI.rdData  := spiPeripheral.io.rdData
-  io.SPI.ack     := spiPeripheral.io.ack
+
 }
--- src/test/scala/PipeConTest.scala
+++ src/test/scala/PipeConTest.scala
@@ -2,184 +2,33 @@ import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
-class PipeConExampleTest extends AnyFlatSpec with ChiselScalatestTester {
-  behavior of "PipeConExample"
+class PipeConInterconnectTest extends AnyFlatSpec with ChiselScalatestTester {
+  "PipeConInterconnect" should "instantiate correctly with a file" in {
+    // Path to your file, relative to the project root or use absolute path
+    val testFile = "C:\\Users\\Andreas\\OneDrive\\kandidat\\Intro_Chip\\dtu-soc-2025\\src\\main\\scala\\c\\hellos.bin" // ensure this file exists
 
-    it should "test assembly code" in {
-      test(new PipeConExample(8)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-        c.io.uartWrDataTest.poke("hDEADBEEF".U) // Set UART data
-        c.io.uartWr.poke(true.B)
-        c.io.uartWrMask.poke("h1".U)
-        c.clock.step(5)
+    test(new PipeConInterconnect(testFile, addrWidth = 32)) { c =>
+      // You can poke and expect c.io signals here if they exist
+      // Example:
+      // c.io.someInput.poke(42.U)
+      // c.io.someOutput.expect(42.U)
+        c.clock.setTimeout(0)
 
-        println(s"SPI Write Data: ${c.io.SPIWrData.peek()}")
-        c.io.SPIWrData.expect("hDEADBEEF".U) // SPI should receive the same data
-      }
+        for (i <- 0 until 100) {
+          c.clock.step(1)
+          val address = c.io.uart.address.peek()
+          println(s"Address value is: $address") 
+          if (c.io.uart.wr.peek() == true.B) {
+            val address = c.io.uart.address.peek()
+            println(s"Address value is: $address")   
+            val wrData = c.io.uart.wrData.peek()
+            println(s"WrData value is: $wrData") 
+          }       
+        }
+        
+
+      // For now, we just verify it instantiates and doesn't crash
+      println("PipeConInterconnect instantiated successfully.")
     }
-
-
-  //it should "simulate a UART write and read" in {
-  //test(new PipeConExample(8)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-  //    val uartAddress = 0x01
-//
-  //    // Perform a read operation from the CPU
-  //    c.io.uartRdDataTest.poke("h0000BEEF".U)  // Simulate data being available to read
-  //    c.io.uartRd.poke(true.B)  // Activate UART read signal
-  //    c.clock.step()
-//
-  //    // Monitor the UART read signal from the interconnect
-  //    var readDetected = false
-  //    while (!readDetected) {
-  //      c.clock.step(1)
-  //      if (c.io.uartRd.peek().litToBoolean) {  // Check if UART read signal is active
-  //        println("UART read detected")
-  //        readDetected = true
-  //      } else {
-  //        println("Nothing detected...")
-  //      }
-  //    }
-//
-  //    //var readDetected2 = false
-  //    //while (!readDetected2) {
-  //    //  c.clock.step(1)
-  //    //  if (c.interconnect.io.rdEnableTest.peek().litToBoolean) {
-  //    //    println("rdEnable from CPU detected")
-  //    //    readDetected2 = true
-  //    //  } else {
-  //    //    println("Nothing detected...")
-  //    //  }
-  //    //}
-  //  }
-  //}
-
-
-  //it should "handle read without write to the UART peripheral" in {
-  //  test(new PipeConExample(8)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-  //    val uartAddress = 0x01
-//
-  //    // Perform a read operation without any prior write
-  //    c.io.cpuAddress.poke(uartAddress.U)
-  //    c.io.cpuRd.poke(true.B)
-  //    c.clock.step()
-//
-  //    // Check that the UART did not return any unexpected data
-  //    c.io.cpuRdData.expect(0.U)
-  //  }
-  //}
-//
-  //it should "perform multiple write and read operations to the UART peripheral" in {
-  //  test(new PipeConExample(8)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-  //    val uartAddress = 0x01
-//
-  //    // First Write
-  //    c.io.cpuAddress.poke(uartAddress.U)
-  //    c.io.cpuWrMask.poke("b0011".U)
-  //    c.io.cpuWrData.poke("h0000AAAA".U)
-  //    c.io.cpuRd.poke(false.B)
-  //    c.clock.step()
-//
-  //    c.io.uartWrMask.expect("b0011".U)
-  //    c.io.uartWrData.expect("h0000AAAA".U)
-//
-  //    // Second Write
-  //    c.io.cpuWrMask.poke("b0011".U)
-  //    c.io.cpuWrData.poke("h0000BBBB".U)
-  //    c.clock.step()
-//
-  //    c.io.uartWrMask.expect("b0011".U)
-  //    c.io.uartWrData.expect("h0000BBBB".U)
-  //    c.clock.step()
-//
-  //    // Reset write signal
-  //    c.io.cpuWrMask.poke(0.U)
-  //    c.clock.step()
-//
-  //    // Third Write
-  //    c.io.cpuWrMask.poke("b0011".U)
-  //    c.io.cpuWrData.poke("h0000CCCC".U)
-  //    c.clock.step()
-//
-  //    // Read from UART again
-  //    c.io.cpuRd.poke(true.B)
-  //    c.clock.step()
-//
-  //    // Expect the third written value
-  //    c.io.cpuRdData.expect("h0000CCCC".U)
-  //  }
-  //}
-//
-  //it should "perform write and read with different data sizes" in {
-  //  test(new PipeConExample(8)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-  //    val uartAddress = 0x01
-//
-  //    // Write 8-bit data
-  //    c.io.cpuAddress.poke(uartAddress.U)
-  //    c.io.cpuWrMask.poke("b0001".U)
-  //    c.io.cpuWrData.poke("hAA".U)
-  //    c.clock.step()
-//
-  //    // Read back 8-bit data
-  //    c.io.cpuWrMask.poke(0.U)
-  //    c.io.cpuRd.poke(true.B)
-  //    c.clock.step()
-  //    c.io.cpuRdData.expect("hAA".U)
-  //    c.io.cpuRd.poke(false.B)
-//
-  //    // Write 16-bit data
-  //    c.io.cpuAddress.poke(uartAddress.U)
-  //    c.io.cpuWrMask.poke("b0011".U)
-  //    c.io.cpuWrData.poke("hBBBBBBBB".U)
-  //    c.clock.step()
-//
-  //    // Read back 16-bit data
-  //    c.io.cpuWrMask.poke(0.U)
-  //    c.io.cpuRd.poke(true.B)
-  //    c.clock.step()
-  //    c.io.cpuRdData.expect("h0000BBBB".U)
-  //    c.io.cpuRd.poke(false.B)
-//
-  //    // Write 32-bit data
-  //    c.io.cpuAddress.poke(uartAddress.U)
-  //    c.io.cpuWrMask.poke("b1111".U)
-  //    c.io.cpuWrData.poke("hCCCCCCCC".U)
-  //    c.clock.step()
-//
-  //    // Read back 32-bit data
-  //    c.io.cpuWrMask.poke(0.U)
-  //    c.io.cpuRd.poke(true.B)
-  //    c.clock.step()
-  //    c.io.cpuRdData.expect("hCCCCCCCC".U)
-  //    c.io.cpuRd.poke(false.B)
-//
-  //    // Write 24-bit data
-  //    c.io.cpuAddress.poke(uartAddress.U)
-  //    c.io.cpuWrMask.poke("b0111".U)
-  //    c.io.cpuWrData.poke("hFFFFFF".U)
-  //    c.clock.step()
-//
-  //    // Read back 24-bit data
-  //    c.io.cpuWrMask.poke(0.U)
-  //    c.io.cpuRd.poke(true.B)
-  //    c.clock.step()
-  //    c.io.cpuRdData.expect("hFFFFFF".U)
-  //    c.io.cpuRd.poke(false.B)
-  //  }
-  //}
-//
-  //it should "handle write with wrMask and read the correct data" in {
-  //  test(new PipeConExample(8)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-  //    val SPIAddress = 0x02
-//
-  //    c.io.cpuAddress.poke(SPIAddress.U)
-  //    c.io.cpuWrMask.poke("b0111".U)
-  //    c.io.cpuWrData.poke("hFFFFFFFF".U)
-  //    c.clock.step()
-//
-  //    c.io.cpuWrMask.poke(0.U)
-  //    c.io.cpuRd.poke(true.B)
-  //    c.clock.step()
-  //    c.io.cpuRdData.expect("h00FFFFFF".U)
-  //    c.io.cpuRd.poke(false.B)
-  //  }
-  //}
+  }
 }
